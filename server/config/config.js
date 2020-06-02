072cf81747f56460e66a99e7766a859d7c93c3d8
Felipe <felipe.donoso.cordero29@gmail.com>
2020-06-01 20:29:11 -0400

    Configuracion DB

--- server/config/config.js
+++ server/config/config.js
@@ -9,3 +9,31 @@ process.env.PORT = process.env.PORT || 5000
 //============================================
 
 process.env.SEED = process.env.SEED || 'semilla-desarrollo-ipronect' // Se debe configurar en produccion
+
+
+// ==========================================
+// Entorno
+// ==========================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+
+// ==========================================
+// DB y URL_EMAIL
+// ==========================================
+
+let urlDB;
+let url;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/iPronect';
+    url = 'http://localhost:5000';
+} else {
+    urlDB = process.env.MONGO_URI;
+    url = process.env.URL;
+}
+
+process.env.URL_DB = urlDB;
+process.env.URL_NETFRUIT = url;
