865becb6e87986545962ab500e42dd70a3e18f1a
Felipe <felipe.donoso.cordero29@gmail.com>
2020-06-15 13:57:44 -0400

    CORS

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
 // Puerto de conexion base de datos
 //============================================
 
-process.env.PORT = process.env.PORT || 5000
+process.env.PORT = process.env.PORT || 3000
 
 //============================================
 // SEED
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
+
+
 
 
 // parse application/x-www-form-urlencoded
@@ -11,28 +13,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-//Direccionamiento de peticiones
-app.use(require('../server/routes/index'));
 
-// CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Osrigin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'token', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
 
 
+/* app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "token,Origin, X-Requested-With, Content-Type, Accept");
+    next();
+}); */
+
+app.use(require('../server/routes/index'));
+
 app.get('/', (req, res) => {
-    res.json('iPronect');
+    res.json('iPronect')
 });
 
-mongoose.connect(process.env.URL_DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err, res) => {
-    if (err) {
-        throw (err)
-    } else { console.log('Base de datos correctamente conectada'); }
-});
 
+mongoose.connect(process.env.URL_DB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (error, res) => {
+        if (error) throw (error);
+
+        else console.log('Base de datos online');
+
+    });
 app.listen(process.env.PORT, () => {
-    console.log(`Escuchando en el puerto ${process.env.PORT}`);
+    console.log(`Escuchando ${process.env.PORT} port`);
+
 });
